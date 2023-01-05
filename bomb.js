afc7b2ca6ebac88237f0e67999b3a455d8b4ed3e
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-05 13:09:50 +0200

    Create bomb.js

---
+++ bomb.js
@@ -0,0 +1,42 @@
+let field = [
+  [" ", "x", " ", "x", " "],
+  [" ", "x", " ", "x", " "],
+  [" ", " ", " ", "x", " "],
+  [" ", "x", " ", "x", " "],
+  [" ", "x", " ", "x", " "],
+];
+
+function bomb(x,y,arr) {
+    if( !(x<0 ||
+        x>=arr.length ||
+        y < 0 ||
+        y>=arr[x].length) ) 
+    {
+        const copy = JSON.parse(JSON.stringify(arr));
+        copy[x][y] = "$";
+        console.table(copy);
+    }
+    
+
+    if(
+        x<0 ||
+        x>=arr.length ||
+        y < 0 ||
+        y>=arr[x].length ||
+        arr[x][y] === 'x' || 
+        arr[x][y] === '@'       
+    ) {
+        return;
+    } 
+
+    arr[x][y] = '@';
+    bomb(x+1,y,arr); // down
+    bomb(x-1,y,arr); // up
+    bomb(x,y+1,arr); // right
+    bomb(x, y-1,arr);// left
+
+}
+
+bomb(2, 2, field);
+
+console.table(field);
