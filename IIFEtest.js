195f4d03a58fdfb99ec88289c751bd6c782a5a91
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-05 10:31:55 +0200

    Update IIFEtest.js

--- IIFEtest.js
+++ IIFEtest.js
@@ -1,8 +1,9 @@
 let global = "Hello";
 
-(function something(data) {
-    console.log("This is test!");
-    var b = "Nothing";
-    console.log(data);
-    console.log(global);
-})("text");
+function something(data) {
+    console.log("hello");
+    return console.log(data);
+    
+};
+
+console.log(something(5));
