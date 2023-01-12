1435fa918546450a92992ad54e066a943e7ad7db
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-12 09:41:08 +0200

    Create testS14-Dices.js

---
+++ testS14-Dices.js
@@ -0,0 +1,16 @@
+let biggestRoll = 0;
+let tryes = 0;
+
+while(biggestRoll < 6){
+    let rollOne = Math.ceil(Math.random()*6);
+    let rollTwo = Math.ceil(Math.random()*6);
+    let rollThree =  Math.ceil(Math.random()*6);
+
+    if(rollOne + rollTwo + rollThree === 18){
+        biggestRoll++;
+    }
+    tryes++;
+}
+
+console.log(`Отне ${tryes} опита да хвърля ${biggestRoll} последователни зара с
+максимален брой!`);
