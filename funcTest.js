43b1f0688d9f36528ccb426d5405b63219c8fa06
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-12 12:35:48 +0200

    Update funcTest.js

--- funcTest.js
+++ funcTest.js
@@ -1,9 +1,20 @@
-function something(a,b,c) {
-    return c(a) + c(b);
+// function something(a,b,c) {
+//     return c(a) + c(b);
+// }
+
+// let beta = (param) => {
+//     return param.length;
+// }
+
+// console.log(something("Nikola", "Sirakov", beta));
+
+let a = 6;
+let b = 3;
+
+function sum(one,two){
+    let c = 2;
+    console.log(c);
+    return one + two;
 }
 
-let beta = (param) => {
-    return param.length;
-}
-
-console.log(something("Nikola", "Sirakov", beta));
+sum(a,b);
