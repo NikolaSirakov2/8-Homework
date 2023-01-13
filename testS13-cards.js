24c8821b5e07801415efe5481f698af509335092
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-13 08:11:40 +0200

    Update testS13-cards.js

--- testS13-cards.js
+++ testS13-cards.js
@@ -1,10 +1,23 @@
-let input = x;
+
 let pairs = 0;
 let count = 0;
-let validCards = "23456789TJQKA";
+let validCards = "123456789PETJQKA";
 
-let array = input.split(" ");
+while(pairs < 4){
+    let cardOne = validCards[Math.ceil(Math.random())*17 - 1];
+    let cardTwo = validCards[Math.ceil(Math.random())*17 - 1];
+    let cardThree = validCards[Math.ceil(Math.random())*17 - 1];
+    let cardFour = validCards[Math.ceil(Math.random())*17 - 1];
+    let cardFive = validCards[Math.ceil(Math.random())*17 - 1];
+    let input = "" + cardOne + cardTwo + cardThree + cardFour + cardFive;
 
-for(let i = 0; i < array.length; i++){
-    
+    console.log(input);
+
+    pairs++;
 }
+
+// let array = input.split(" ");
+
+// for(let i = 0; i < array.length; i++){
+    
+// }
